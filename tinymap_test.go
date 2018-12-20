645674a93dd3d49b7e441560b237fea331cbcc00
selfup <boudinot.regis@yahoo.com>
2018-12-20 06:18:56 -0600

    better bench case

--- tinymap_test.go
+++ tinymap_test.go
@@ -5,7 +5,13 @@ import "testing"
 func Benchmark_Get(b *testing.B) {
 	tinyStrMap := new(TinyStrMap)
 
-	tinyStrMap.Set("a", "b")
+	tinyStrMap.Set("z", "_")
+	tinyStrMap.Set("f", "_")
+	tinyStrMap.Set("v", "_")
+	tinyStrMap.Set("t", "_")
+	tinyStrMap.Set("a", "_")
+	tinyStrMap.Set("j", "_")
+	tinyStrMap.Set("x", "_")
 
 	for n := 0; n < b.N; n++ {
 		tinyStrMap.Get("a")
