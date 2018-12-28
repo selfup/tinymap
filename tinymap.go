8ba986993dd5b137cffb9e7de9d27902be97989d
selfup <boudinot.regis@yahoo.com>
2018-12-28 17:41:55 -0600

    update README and package info

--- tinymap.go
+++ tinymap.go
@@ -1,5 +1,5 @@
 // Package tinymap provides a simple to use interface that behaves like a HashMap.
-// Multiple "Maps" are exposed for use as well as structs that behave like "Tuples"
+// Multiple "Maps" are exposed for use as well as structs that behave like "Tuples".
 // This package is intended to be used with tinygo, but has some nice little structs.
 // Feel free to use this as you wish. It is very small, but helps with embedded programming.
 package tinymap
