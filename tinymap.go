c6e42779f0a7bc4f33627017987c8e310ba49e82
selfup <boudinot.regis@yahoo.com>
2018-12-28 17:38:55 -0600

    add package level declaration

---
+++ tinymap.go
@@ -0,0 +1,4 @@
+// Package tinymap provides a simple to use interface that behaves like a HashMap.
+// This package is inteded to be used with tinygo, but has some nice little structs.
+// Feel free to use this as you wish. It is very small, but helps with embedded programming.
+package tinymap
